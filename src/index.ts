cf95e1bfff14e3df9cf567a32d96c0fd86470cf9
Nato Boram <nato.boram+git@gmail.com>
2024-04-25 23:28:31 -0400

    📝 Document index.ts and main.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+/**
+ * If you are making a library, this is where you should export everything.
+ * Having a single entry point at `index.ts` gives you total control over what
+ * you export to the outside world.
+ *
+ * You can make the task easier by adding `index.ts` files in each folder
+ * specifying exactly what you want to expose and having the `index.ts` in the
+ * parent folders re-export everything from them.
+ *
+ * Reserve all `index.ts` files to the single task of exporting to the outside
+ * world. Do not declare anything in them.
+ *
+ * @module
+ */
+
 export * from "./main.js"
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
+/**
+ * If you are making a command-line application, this should be the starting
+ * point.
+ *
+ * You can start the project in dev mode with `pnpm run dev` or in production
+ * mode with `pnpm run build && pnpm run start`.
+ *
+ * @module
+ */
+
 console.log("Hello, world!")
